8c60b85432d6b3a633d03fd0aa6a2d6b2d9005dd
Tryd0g0lik <work80@mail.ru>
2022-10-05 18:24:48 +0700

    Adding '.env', 'config.py' files

--- orm_migrations/website/settings.py
+++ orm_migrations/website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from config import PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,6 +75,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
+        'PASSWORD' : PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
