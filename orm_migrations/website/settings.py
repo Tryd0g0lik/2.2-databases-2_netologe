8e440881aabfc4e04f7a0d964f10b9a4f561de79
Tryd0g0lik <work80@mail.ru>
2022-10-06 16:29:07 +0700

    No migrations to apply.

--- orm_migrations/website/settings.py
+++ orm_migrations/website/settings.py
@@ -75,7 +75,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
-        'PASSWORD' : 'nlo7',
+        'PASSWORD' : '....',
+        'USER':'....',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
